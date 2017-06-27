2f46733cba7cfa2731afee836fabb2902ba777f1
Dushyant <6dushyantmishra20101@gmail.com>
2017-06-27 22:01:14 +0530

    Added DEG, DDEG solution

---
+++ scripts/DEG.py
@@ -0,0 +1,12 @@
+n,e = map(int, raw_input().strip().split())
+graph = {}
+for i in range(n):
+	graph[i] = []
+for a in range(e):
+	v1, v2 = map(int, raw_input().strip().split())
+	graph[v1-1].append(v2-1)
+	#print graph
+	graph[v2-1].append(v1-1)
+#print graph
+for a in range(n):
+	print len(graph[a]), 
