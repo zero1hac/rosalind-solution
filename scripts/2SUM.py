11072ec65611aceefe3cd8f5c3b8b0100d3b1826
Dushyant <6dushyantmishra20101@gmail.com>
2017-07-04 01:00:00 +0530

    Added 2SUM solution

--- scripts/2SUM.py
+++ scripts/2SUM.py
@@ -1,17 +1,19 @@
-import sys
-file_input = open(sys.argv[1], "rb")
-lines = file_input.readlines()
-k, n = map(int, lines[0].strip().split())
-print k,n
-print lines
-for i in range(1,k+1):
-    line = lines[i]
-    flag = 0
-    array_ = map(int, line.strip().split())
-    print array_
-    for j in range(n):
-        if -1*array_[j] in array_[j:]:
-            print j+1, array_.index(-1*array_[j]) + 1
-            flag=1
-    if flag==0:
-        print -1
+def print_arr(A):
+    dict1 = {}
+    for i in range(len(A)):
+        try:
+            k = dict1[-1 * A[i]]
+            return (k, i)
+        except:
+            dict1[A[i]] = i
+    return (-1,-1)
+
+if __name__ == '__main__':
+    n , m = map(int, raw_input().strip().split())
+    for i in range(n):
+        arr = map(int, raw_input().strip().split())
+        t = print_arr(arr)
+        if t[0]!=-1:
+            print t[0]+1, t[1]+1
+        else:
+            print "-1"
